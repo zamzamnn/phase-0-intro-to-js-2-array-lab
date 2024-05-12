b561c80e8144aebfc44cff6274ce359f65cec7b2
zamzamnn <zamzamnoorahmed73@gmail.com>
2024-05-12 14:02:23 +0300

    cpmplete lab aray

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Write your solution here!
+const cats =  ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name){
+cats.push ('Ralph');  
+}
+function destructivelyPrependCat(name){
+    cats.unshift("Bob");
+}
+function destructivelyRemoveLastCat(){
+        cats.pop( "Garfield");
+}
+   function destructivelyRemoveFirstCat(){
+    cats.shift("Milo");
+   }function appendCat(name){
+    return [...cats , name]
+   }
+   function prependCat(name){
+    return [name , ...cats]
+   }
+   function removeLastCat() {
+    return  cats.slice(0, -1);
+   }function removeFirstCat() {
+    return cats.slice(1);
+   }
+   
+    
+    
+
+
+
+
+
+
+    
+
+    
+
+
+    
+
